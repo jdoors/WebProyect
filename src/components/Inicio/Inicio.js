35a5efe3dab499910816cbf3db53ffe666061ad8
Martin Facchinello <martinfac@hotmail.com>
2022-03-01 11:23:51 -0300

    Creación de hoja Inicio, para poder ir cargando cada componente creado.

---
+++ src/components/Inicio/Inicio.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Inicio = () => {
+  return (
+    <div>En esta página tendríamos que ir añadiendo todos los componentes que vamos creando en base al Figma, menos la navbar, la sección de redes sociales y el footer</div>
+  )
+}
+
+export default Inicio
