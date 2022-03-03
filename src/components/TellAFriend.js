76612ed3503ca1dc30223d5ea72054c50ffc160d
Andres Gottau <andres.mdp@hotmail.com>
2022-03-03 19:02:18 -0300

    agrego TellAFriend.js

---
+++ src/components/TellAFriend.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const TellAFriend = () => {
+    return (
+        <div>
+            <p>Tell a friend</p>
+            <img/>
+        </div>
+        
+    )
+}
+
+export default TellAFriend
