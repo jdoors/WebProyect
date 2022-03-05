a29e425e257dba25607c5d768c39caef7c40e216
jdoors <jesicapuertas@hotmail.com>
2022-03-04 21:42:43 -0300

    imagen logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import imagen from './Imagenes/LogoNavbar.jpg'
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <div>
+
+      <div>
+        <img src={imagen}></img>
+        <h2>Pet Shop Fullstack</h2>
+      </div>
+
+      <ul>
+        <li>Inicio</li>
+        <li>Perros</li>
+        <li>Gatos</li>
+        <li>Contacto</li>
+      </ul>
+
+    </div>
   )
 }
 
