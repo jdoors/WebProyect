20334e0b586bde2e650fd2aa02c750c50cb15798
jdoors <jesicapuertas@hotmail.com>
2022-03-05 09:01:31 -0300

    Estilos a la Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,20 +3,23 @@ import imagen from './Imagenes/LogoNavbar.jpg'
 
 const Navbar = () => {
   return (
+    <div className='items-center justify-between flex bg-[#E5E5E5] px-12 py-1 my-0 mx-auto '>
+
+      <div className='items-center'>
+        <img src={imagen} className='w-24 rounded-full pt-2' ></img>
+        <h2 className='font-poppins mx-mp'>Pet Shop Fullstack</h2>
+
+    </div>
+
     <div>
-
-      <div>
-        <img src={imagen}></img>
-        <h2>Pet Shop Fullstack</h2>
-      </div>
-
-      <ul>
-        <li>Inicio</li>
-        <li>Perros</li>
-        <li>Gatos</li>
-        <li>Contacto</li>
-      </ul>
-
+    <ul className='flex space-x-4 font-poppins' >
+        <li><a href='#'>Inicio</a></li>
+        <li><a href='#'>Perros</a></li>
+        <li><a href='#'>Gatos</a></li>
+        <li><a href='#'>Contacto</a></li>
+      </ul> 
+    </div>
+    
     </div>
   )
 }
