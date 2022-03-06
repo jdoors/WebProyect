3382223fb6db691522996b907b7cc2705af22e6c
Andres Gottau <andres.mdp@hotmail.com>
2022-03-06 15:22:43 -0300

    fuente y centrado

--- src/components/RedesSociales/RedesSociales.js
+++ src/components/RedesSociales/RedesSociales.js
@@ -3,9 +3,16 @@ import RedesSociales from "./Imagen/RedesSociales.jpg";
 
 const SocialMedia = () => {
     return (
-        <div className='mt-10'>
-            <h2 className='text-4xl font-bold text-center title-ultimas-ofertas'>Compartilo con tus amigos</h2>
-            <img src={RedesSociales} />
+        <div className='mt-10 relative flex flex-wrap justify-evenly'>
+            <div className='font-cormorant font-bold text-6xl top-[50%] absolute'>
+                <h1>
+                    Compartilo con tus amigos
+                </h1>
+            </div>
+            <div className="">
+                <img className="" src={RedesSociales} />
+            </div>
+            
         </div>
         
         
