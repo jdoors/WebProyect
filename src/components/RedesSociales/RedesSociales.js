397678b5ac308442fa5559c0f1b215cdfa2544f6
Andres Gottau <andres.mdp@hotmail.com>
2022-03-07 22:24:23 -0300

    filtro, ancho segun pantalla, color texto

--- src/components/RedesSociales/RedesSociales.js
+++ src/components/RedesSociales/RedesSociales.js
@@ -4,15 +4,16 @@ import RedesSociales from "./Imagen/RedesSociales.jpg";
 const SocialMedia = () => {
     return (
         <div className='mt-10 relative flex flex-wrap justify-evenly'>
-            <div className='font-cormorant font-bold text-6xl top-[50%] absolute'>
+            <div>
+                <img className="object-cover h-[260px] w-screen filter grayscale" src={RedesSociales} />
+            </div>
+
+            <div className='font-cormorant text-5xl font-[1000] top-[20%] absolute text-[#FF8126] text-center'>
                 <h1>
                     Compartilo con tus amigos
                 </h1>
             </div>
-            <div className="">
-                <img className="" src={RedesSociales} />
-            </div>
-            
+        
         </div>
         
         
