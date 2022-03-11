f8e070388f9e8a49c52d25c51b3fba34800f5c07
Belen <mbelen03@live.com>
2022-03-11 20:54:03 -0300

    cambio de logo

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logoMascota from '../Footer/Imagenes/logomascota.jpg';
+import logoMascota from '../Footer/Imagenes/logonaranja.jpg';
 
 const Footer = () => {
   return (
-    <div class="w-full bg-slate-100 text-black">
+    <div class="w-full bg-white text-black">
       <div class="xl:px-20 pb-6 lg:px-20 md:px-10 sm:px-5 px-10">
       <div class="w-full pt-12 flex flex-col sm:flex-row space-y-2 justify-end">
         <div class="w-full sm:w-1/4 pt-2 flex flex-col space-y-4">
-          <img className="object-cover" src={logoMascota} alt="logo mascota"/> 
+          <img className="bg-" src={logoMascota} alt="logo mascota"/> 
         </div>
         <div class="w-full sm:w-1/4 px-12 flex flex-col space-y-4">
-          <h2 class="font-poppins">Sobre Nosotros</h2>
+          <a class="font-poppins font-medium text-black " href= "#">Sobre Nosotros</a>
+          <ol className='text-slate-600'>
+            <li>Quiénes somos</li>
+            <li>Concejos para tu mascota</li>
+            <li>Preguntas frecuentes</li>
+          </ol>
+          
         </div>
         <div class="w-full sm:w-1/4 px-12 flex flex-col space-y-4">
-          <h2 class="font-poppins">Productos</h2>
+          <a class="font-poppins font-medium text-black" href="#">Productos</a>
+          <ol className='text-slate-600'>
+            <li>Perros</li>
+            <li>Gatos</li>
+          </ol>
         </div>
         <div class="w-full sm:w-1/4 px-12 flex flex-col space-y-4">
-          <h2 class="font-poppins">Contacto</h2>
+          <a class="font-poppins font-medium text-black" href="#">Contacto</a>
         </div>
-      </div>
-      <div class="opacity-60 pt-2">
-      <p>© 2022 Proyecto React.</p>
       </div>
       </div>
     </div>
