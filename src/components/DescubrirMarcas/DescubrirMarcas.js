2bb3bcfab67429d7dd7713bce940b2d5f1ecd84d
Andres Gottau <andres.mdp@hotmail.com>
2022-03-11 23:03:23 -0300

    cambio estilos, colores, resp

--- src/components/DescubrirMarcas/DescubrirMarcas.js
+++ src/components/DescubrirMarcas/DescubrirMarcas.js
@@ -4,23 +4,23 @@ import DescubrirMarcas from "./Imagen/DescubrirMarcas.jpeg";
 const DMarca = () => {
     return (
 
-        <div className="relative flex">
-            
-            <div className="justify-between font-cormorant bg-gradient-to-r from-blue-200">
-                <h1 className="text-6xl font-[700] ml-[55px] mt-[20%]">Encontra las mejores marcas para tu mascota</h1>
+        <div className="flex-row w-screen md:flex">
+                
+            <div className="h-[600px] object-center font-cormorant bg-gradient-to-b from-blue-200 md:bg-gradient-to-r from-blue-200">
                 <br/>
-                <h2 className="text-3xl font-poppins ml-[55px]">Descubri marcas y sus historias</h2>
+                <h1 className="text-3xl md:text-6xl font-[700] ml-[55px] mt-[20%]">Encontra las mejores marcas para tu mascota</h1>
                 <br/>
-                <button className='bg-[#FF8126] text-white rounded w-[200px] h-11 mb-6 font-poppins ml-[55px]'>
+                <h2 className="text-xl md:text-3xl font-poppins ml-[55px]">Descubri marcas y sus historias</h2>
+                <br/>
+                <button className='border-2 border-black text-black rounded w-[200px] h-11 font-poppins ml-[55px]'>
                     Buscar Marcas
                 </button>
             </div>
 
-                
-            <div className="justify-between ml-[20%] flex">
-                <img className="object-cover" src={DescubrirMarcas}/>
+            <div className="">
+                    <img className="object-none object-left h-[600px]" src={DescubrirMarcas}/>
             </div>
-        
+            
         </div>
     )
 }
