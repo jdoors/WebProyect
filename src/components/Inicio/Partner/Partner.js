24994d22f83d117ea884d73380609aa2aaac7fec
Martin Facchinello <martinfac@hotmail.com>
2022-03-25 16:42:31 -0300

    Creación componente Partner (Royal Canin)

---
+++ src/components/Inicio/Partner/Partner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import fondoRoyalCanin from './Images/Fondo.png'
+import PartnerRoyalCanin from './Images/RoyalCaninLogo.png'
+
+const Partner = () => {
+
+  return (
+    <div className='w-full h-[300px] sm:h-[395px] bg-[#6090C3]' style={{backgroundImage: `url(${fondoRoyalCanin})`}}>
+        <div className='flex flex-col items-center'>
+            <h2 className='font-cormorant text-3xl text-center sm:text-[40px] font-bold text-white -mb-7 mt-14'>Distribuidor Oficial de Royal Canin</h2>
+            <img className='w-44 sm:w-64 -mb-7' src={PartnerRoyalCanin}/>
+            <button className='bg-[#FF8126] text-white rounded w-60 sm:w-[330px] h-8 sm:h-11 text-sm sm:text-lg font-medium font-poppins'>Ver productos exclusivos</button>
+        </div>
+    </div>
+  )
+}
+
+export default Partner
