2ee70f750f8559a16f6bce551133e319f580dba7
PablaBertani <83288454+PablaBertani@users.noreply.github.com>
2022-03-25 10:01:08 -0300

    Modificacion de medidas para mobile first

--- src/components/Inicio/SpaDay/SpaDay.js
+++ src/components/Inicio/SpaDay/SpaDay.js
@@ -3,15 +3,15 @@ import FrenchBulldogPuppy from './Images/FrenchBulldogPuppy.jpeg'
 
 const SpaDay = () => {
   return (
-    <div className=' grid grid-cols-2 justify-items-center mt-8 mx-14 '>
+    <div className=' grid  auto-rows-min md:grid-cols-2 md:justify-items-center mt-8 mx-14 '>
 
-      <div>
-        <img className=' w-[560px] h-[390px]  opacity-100' src={FrenchBulldogPuppy} alt='Bulldog Puppy' />
+      <div >
+        <img className='w-[500px] h-[350px] opacity-100 md:w-[560px] md:h-[390px]' src={FrenchBulldogPuppy} alt='Bulldog Puppy' />
       </div>
 
-      <div> 
-        <h1 className='font-cormorant font-bold text-4xl  text-[#FF8126] mt-10'> Sorprendelos</h1>
-        <h1 className='font-cormorant font-bold text-4xl  '> Tu mascota más relajada que nunca</h1>
+      <div className='md:ml-3 mt-10'> 
+        <h1 className='font-cormorant font-bold text-4xl text-[#FF8126]'> Sorprendelos</h1>
+        <h2 className='font-cormorant font-bold text-4xl'> Tu mascota más relajada que nunca</h2>
         
         <p className='font-poppins font-medium not-italic text-xs leading-6 mt-3' >
         Mima a tu mascota con este kit especializado en un spa para perros.
@@ -19,10 +19,12 @@ const SpaDay = () => {
         una esponja quitapelusas y fragancias que propiciarán un ambiente calmado para que el perro y su familia disfruten de un agradable momento de tranquilidad.
         </p>
         <p className='font-poppins font-medium not-italic text-xs leading-6'> ¡Este paquete de spa es un regalo ideal para mostrarle a tu mascota cuánto lo amas!</p>
-        <button className='bg-[#FF8126] text-white rounded w-[230px] h-10 mt-8 mb-6'>Comprar</button>
-        </div >
+        <button className='bg-[#FF8126] text-white rounded w-[230px] h-10 mt-4 md:mt-8 mb-6'>Comprar</button>
+        
+      </div >
 
-      </div>
+      
+    </div>
   )
 }
 
