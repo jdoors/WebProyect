759ccfe40556979f9c192a37b704bd9edcdb61da
PablaBertani <83288454+PablaBertani@users.noreply.github.com>
2022-03-31 10:58:05 -0300

    Instalación de Formik ycuerpo de formulario

--- src/components/Pages/Contactos.js
+++ src/components/Pages/Contactos.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import {Formik,Form,Field, ErrorMessage} from 'formik'
 
 const Contactos = () => {
-return (
-    <div>Hola desde Contactos</div>
-)
+  return (
+    <div>
+        <p>hi!</p>
+        <Formik>
+            {()=> (
+        <Form>
+            <div>
+                <label htmlFor='name'>Nombre</label>
+                <Field type='text' id='name' name='name' placeholder='Nombre'/>
+            </div>
+            <div>
+                <label htmlFor='email'>e_mail</label>
+                <Field type='email' id='email' name='email' placeholder='e_mail'/>
+            </div>
+            <div>
+                <label htmlFor='phone'>Teléfono</label>
+                <Field type='text' id='phone' name='phone' placeholder='Teléfono'/>
+            </div>
+
+            <div>
+                 <Field name='asunto' as='select'>
+                     <option value='consulta'>Consulta </option>
+                     <option value='reclamo'>Reclamo </option>
+                     <option value='otros'>Otros </option>
+                 </Field>
+            </div>
+            <div>
+                 <Field name='textarea' as='textarea' placeholder='Mensaje'/>
+            </div>
+
+            <button type='submit'>Enviar</button>
+        </Form>
+        )}
+        </Formik>
+    </div>
+  )
 }
 
 export default Contactos
