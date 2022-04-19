e7ddab35b6cd9a1bfc9ffe82c124e8700fb8fd67
Martin Facchinello <martinfac@hotmail.com>
2022-04-19 11:58:20 -0300

    Arreglo responsive DescrubrirMarcas

--- src/components/Inicio/DescubrirMarcas/DescubrirMarcas.js
+++ src/components/Inicio/DescubrirMarcas/DescubrirMarcas.js
@@ -10,11 +10,11 @@ const DMarca = () => {
             {/* Para md */}
             <div className="h-[600px] object-center w-[70%] font-cormorant hidden md:block bg-gradient-to-r from-blue-200">
                 <br/>
-                <h1 className="text-3xl md:text-6xl font-[700] ml-[55px] mt-[150px]">Encontra las mejores marcas para tu mascota</h1>
+                <h1 className="text-3xl md:text-6xl font-[700] ml-[25px] lg:ml-[55px] mt-[15%] lg:mt-[20%]">Encuentra las mejores marcas para tu mascota</h1>
                 <br/>
-                <h2 className="text-xl md:text-3xl font-poppins ml-[55px]">Descubri marcas y sus historias</h2>
+                <h2 className="text-xl md:text-3xl font-poppins ml-[25px] lg:ml-[55px]">Descubre marcas y sus historias</h2>
                 <br/>
-                <button className='border-2 border-black text-black rounded w-[200px] h-11 font-poppins ml-[55px]'>
+                <button className='border-2 border-black text-black rounded w-[200px] h-11 font-poppins ml-[25px] lg:ml-[55px]'>
                     <Link to="/Productos">Buscar Marcas</Link>
                 </button>
             </div>
